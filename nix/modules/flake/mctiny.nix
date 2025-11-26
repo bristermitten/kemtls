872c548879cc986a5f6703efe3a48c2c2365d7bd
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-11-26 17:46:41 +0000

    fix mctiny nix definition

--- nix/modules/flake/mctiny.nix
+++ nix/modules/flake/mctiny.nix
@@ -9,29 +9,23 @@
             src = ../../../mctiny;
             nativeBuildInputs = [ pkgs.python3 ];
 
-            # 1. OPTIONAL: Speed up compilation
             enableParallelBuilding = true;
 
-            # 2. OPTIONAL: Explicit build flags if needed (uncomment to use)
-            makeFlags = [ "libmctiny.${if final.stdenv.hostPlatform.isDarwin then "dylib" else "so"}" ];
+            makeFlags = [ "libmctiny.${if final.stdenv.hostPlatform.isDarwin then "dylib" else "so"}" "mctiny-test" ];
 
-            # 3. FIXED Install Phase
             installPhase = ''
               mkdir -p $out/lib $out/include $out/bin
         
-              # Nix calculates the extension at evaluation time, so the shell script is simpler
               cp libmctiny.${if final.stdenv.hostPlatform.isDarwin then "dylib" else "so"} $out/lib/
         
               cp mctiny.h $out/include/
               cp crypto_kem_mceliece6960119.h $out/include/
-
-                 cp mctiny-test $out/bin/
+              cp mctiny-test $out/bin/
             '';
             meta.mainProgram = "mctiny-test";
           };
         })
       ];
-      config.allowImportFromDerivation = true;
     };
 
 
